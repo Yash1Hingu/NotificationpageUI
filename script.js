636608c8bfa3912f34e581c7df2cb9db446f111e
Yash1Hingu <yash23hingu@gmail.com>
2023-04-24 17:59:43 +0530

    on particuler notification add read complete script

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const markRead = document.getElementById("markRead");
+const notifications = document.querySelectorAll(".notification.active");
+const counter = document.getElementById("counter");
+counter.innerHTML = notifications.length;
 markRead.addEventListener("click",()=>{
-    const notifications = document.querySelectorAll(".notification.active");
     notifications.forEach((item)=>{
         item.classList.remove("active");
     })
+    counter.style.display = "none";
 })
+notifications.forEach((item)=>{
+    item.addEventListener("click",function(){
+        if(item.classList.contains("active")){
+            item.classList.remove("active");
+            counter.innerHTML -= 1;
+        }
+    })
+})
+
